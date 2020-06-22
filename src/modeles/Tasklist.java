4332d85cd6f1486ba9de171a0cbd432e9393db5e
higa.shouichi <higashouichi@yahoo.co.jp>
2020-06-22 23:15:08 +0900

    WIN

--- src/modeles/Tasklist.java
+++ src/modeles/Tasklist.java
@@ -21,50 +21,60 @@ import javax.persistence.Table;
 
 @Table(name = "tasklist")
 public class Tasklist {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	    @Id
+	    @Column(name = "id")
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer id;
 
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
+	    @Column(name = "title", length = 255, nullable = false)
+	    private String title;
 
-    @Column(name = "created_at", nullable = false)
-    private Timestamp created_at;
+	    @Column(name = "content", length = 255, nullable = false)
+	    private String content;
 
-    @Column(name = "updated_at", nullable = false)
-    private Timestamp updated_at;
+	    @Column(name = "created_at", nullable = false)
+	    private Timestamp created_at;
 
-    public Integer getId() {
-        return id;
-    }
+	    @Column(name = "updated_at", nullable = false)
+	    private Timestamp updated_at;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	    public Integer getId() {
+	        return id;
+	    }
 
+	    public void setId(Integer id) {
+	        this.id = id;
+	    }
 
-    public String getContent() {
-        return content;
-    }
+	    public String getTitle() {
+	        return title;
+	    }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	    public void setTitle(String title) {
+	        this.title = title;
+	    }
 
-    public Timestamp getCreated_at() {
-        return created_at;
-    }
+	    public String getContent() {
+	        return content;
+	    }
 
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
-    }
+	    public void setContent(String content) {
+	        this.content = content;
+	    }
 
-    public Timestamp getUpdated_at() {
-        return updated_at;
-    }
+	    public Timestamp getCreated_at() {
+	        return created_at;
+	    }
 
-    public void setUpdated_at(Timestamp updated_at) {
-        this.updated_at = updated_at;
-    }
-}
+	    public void setCreated_at(Timestamp created_at) {
+	        this.created_at = created_at;
+	    }
+
+	    public Timestamp getUpdated_at() {
+	        return updated_at;
+	    }
+
+	    public void setUpdated_at(Timestamp updated_at) {
+	        this.updated_at = updated_at;
+	    }
+	}
